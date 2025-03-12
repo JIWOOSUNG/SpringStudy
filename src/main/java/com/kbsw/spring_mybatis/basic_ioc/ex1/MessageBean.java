9f7a19580435f0580fd783c6bd017d7e06130e4a
JIWOOSUNG <woosung727@naver.com>
2025-03-12 13:33:35 +0900

    오래된 파일 삭제

--- src/main/java/com/kbsw/spring_mybatis/basic_ioc/ex1/MessageBean.java
+++
@@ -1,8 +0,0 @@
-package com.kbsw.spring_mybatis.basic_ioc.ex1;
-
-public interface MessageBean {
-
-    void sayHello(String name); //spec
-    void sayHi();
-
-}
--- src/main/java/com/kbsw/spring_mybatis/basic_ioc/ex2/Service.java
+++
@@ -1,6 +0,0 @@
-package com.kbsw.spring_mybatis.basic_ioc.ex2;
-
-public interface Service {
-    void test1();
-    void test2();
-}
